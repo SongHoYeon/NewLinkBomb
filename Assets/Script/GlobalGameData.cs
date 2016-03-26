66eeb7e26e772b3b39d871d3d0b2fd57feb5aab4
Song HoYeon <jgghon@gmail.com>
2016-03-26 20:25:37 +0900

    Create project.
    Unreviewed.

---
+++ Assets/Script/GlobalGameData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GlobalGameData {
+    private static GlobalGameData instance = null;
+
+    public static GlobalGameData Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GlobalGameData();
+            }
+
+            return instance;
+        }
+    }
+
+    public int width { get; set;}
+    public int height { get; set; }
+
+    public int totalStageCount { get; set; }
+}
---
+++ Assets/Script/SaveFileMng.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[Serializable]
+class TileDataType
+{
+    public int width;
+    public int height;
+    public List<int> ListMap;
+}
+
+public class SaveFileMng : MonoBehaviour {
+    public void SaveFileForTxt(List<MapToolTileInfo> tileList, string fileName)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.dataPath + "/Resources/SaveDatas/" + fileName + ".dat");
+
+        TileDataType data = new TileDataType();
+        data.width = GlobalGameData.Instance.width;
+        data.height = GlobalGameData.Instance.height;
+
+        data.ListMap = new List<int>();
+
+        List<int> tmpList = new List<int>();
+        foreach (MapToolTileInfo tile in tileList)
+            tmpList.Add((int)tile.Type);
+
+        data.ListMap = tmpList;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+}
