d2641b2cb08662dac202570c7af22f9b6e2aa2b7
Jwa JungWoo LAPTOP <whwjddn1@gmail.com>
2016-03-27 01:09:57 +0900

    Create stage scene.
    Unreviewed.

---
+++ Assets/Script/Chapter/LBChapter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LBChapter : MonoBehaviour {
+
+    float a = 0.0f;
+    public GameObject MovePoint;
+
+	void Start () {
+	    
+	}
+	
+	void Update () {
+	}
+
+    public void OnClickChapter_1(){
+        StartCoroutine("Aplus");
+        transform.position = Vector3.Lerp(transform.position, MovePoint.transform.position, a);
+    }
+
+    IEnumerator Aplus()
+    {
+        a += 0.05f;
+        yield return new WaitForSeconds(0.1f);
+    }
+}
