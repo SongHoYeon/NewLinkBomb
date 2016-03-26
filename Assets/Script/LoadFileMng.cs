89b64f7c80c4cd40f42b9ddfb2a66b973e7c9b1f
Song HoYeon <jgghon@gmail.com>
2016-03-26 21:40:06 +0900

    Fixed bug.
    Unreviewed.

--- Assets/Script/LoadFileMng.cs
+++ Assets/Script/LoadFileMng.cs
@@ -1,61 +1,61 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
-using System.Linq;
+﻿//using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System;
+//using System.Runtime.Serialization.Formatters.Binary;
+//using System.IO;
+//using System.Linq;
 
-public class LoadFileMng : MonoBehaviour
-{
-    private static LoadFileMng instance;
-    private static GameObject container;
+//public class LoadFileMng : MonoBehaviour
+//{
+//    private static LoadFileMng instance;
+//    private static GameObject container;
 
-    public static LoadFileMng Instance()
-    {
-        if (!instance)
-        {
-            container = new GameObject();
-            container.name = "LoadFileMng";
-            instance = container.AddComponent(typeof(LoadFileMng)) as LoadFileMng;
-        }
-        return instance;
-    }
+//    public static LoadFileMng Instance()
+//    {
+//        if (!instance)
+//        {
+//            container = new GameObject();
+//            container.name = "LoadFileMng";
+//            instance = container.AddComponent(typeof(LoadFileMng)) as LoadFileMng;
+//        }
+//        return instance;
+//    }
 
-    public delegate void loadSuccessCallback(List<MapToolTileInfo> list);
-    private loadSuccessCallback successCallback;
+//    public delegate void loadSuccessCallback(List<MapToolTileInfo> list);
+//    private loadSuccessCallback successCallback;
 
-    private List<MapToolTileInfo> tileList = new List<MapToolTileInfo>();
+//    private List<MapToolTileInfo> tileList = new List<MapToolTileInfo>();
 
-    public void Create(loadSuccessCallback _successCallback)
-    {
-        successCallback = _successCallback;
-    }
+//    public void Create(loadSuccessCallback _successCallback)
+//    {
+//        successCallback = _successCallback;
+//    }
 
-    public void LoadFile(string fileName)
-    {
-        if (File.Exists(Application.dataPath + "/Resources/SaveDatas/" + fileName + ".dat"))
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.dataPath + "/Resources/SaveDatas/" + fileName + ".dat", FileMode.Open);
-            TileDataType data = (TileDataType)bf.Deserialize(file);
+//    public void LoadFile(string fileName)
+//    {
+//        if (File.Exists(Application.dataPath + "/Resources/SaveDatas/" + fileName + ".dat"))
+//        {
+//            BinaryFormatter bf = new BinaryFormatter();
+//            FileStream file = File.Open(Application.dataPath + "/Resources/SaveDatas/" + fileName + ".dat", FileMode.Open);
+//            TileDataType data = (TileDataType)bf.Deserialize(file);
 
-            file.Close();
+//            file.Close();
 
-            GlobalGameData.Instance.width = data.width;
-            GlobalGameData.Instance.height = data.height;
+//            GlobalGameData.Instance.width = data.width;
+//            GlobalGameData.Instance.height = data.height;
 
-            tileList.Clear();
-            foreach (int type in data.ListMap)
-            {
-                MapToolTileInfo tile = new MapToolTileInfo();
-                tile.Type = (MapToolTileType)System.Enum.Parse(typeof(MapToolTileType), type.ToString());
-                tileList.Add(tile);
-            }
+//            tileList.Clear();
+//            foreach (int type in data.ListMap)
+//            {
+//                MapToolTileInfo tile = new MapToolTileInfo();
+//                tile.Type = (MapToolTileType)System.Enum.Parse(typeof(MapToolTileType), type.ToString());
+//                tileList.Add(tile);
+//            }
 
-            successCallback(tileList);
-        }
-        else
-            Debug.Log("error : file not found.");
-    }
-}
+//            successCallback(tileList);
+//        }
+//        else
+//            Debug.Log("error : file not found.");
+//    }
+//}
--- Assets/Script/SaveFileMng.cs
+++ Assets/Script/SaveFileMng.cs
@@ -1,37 +1,37 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
+﻿//using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System;
+//using System.Runtime.Serialization.Formatters.Binary;
+//using System.IO;
 
-[Serializable]
-class TileDataType
-{
-    public int width;
-    public int height;
-    public List<int> ListMap;
-}
+//[Serializable]
+//class TileDataType
+//{
+//    public int width;
+//    public int height;
+//    public List<int> ListMap;
+//}
 
-public class SaveFileMng : MonoBehaviour {
-    public void SaveFileForTxt(List<MapToolTileInfo> tileList, string fileName)
-    {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.dataPath + "/Resources/SaveDatas/" + fileName + ".dat");
+//public class SaveFileMng : MonoBehaviour {
+//    public void SaveFileForTxt(List<MapToolTileInfo> tileList, string fileName)
+//    {
+//        BinaryFormatter bf = new BinaryFormatter();
+//        FileStream file = File.Create(Application.dataPath + "/Resources/SaveDatas/" + fileName + ".dat");
 
-        TileDataType data = new TileDataType();
-        data.width = GlobalGameData.Instance.width;
-        data.height = GlobalGameData.Instance.height;
+//        TileDataType data = new TileDataType();
+//        data.width = GlobalGameData.Instance.width;
+//        data.height = GlobalGameData.Instance.height;
 
-        data.ListMap = new List<int>();
+//        data.ListMap = new List<int>();
 
-        List<int> tmpList = new List<int>();
-        foreach (MapToolTileInfo tile in tileList)
-            tmpList.Add((int)tile.Type);
+//        List<int> tmpList = new List<int>();
+//        foreach (MapToolTileInfo tile in tileList)
+//            tmpList.Add((int)tile.Type);
 
-        data.ListMap = tmpList;
+//        data.ListMap = tmpList;
 
-        bf.Serialize(file, data);
-        file.Close();
-    }
-}
+//        bf.Serialize(file, data);
+//        file.Close();
+//    }
+//}
